893dd757ff0229c9bc86fde56101c3e7af902987
Rodrigo Monteiro do Amaral <rodrigoma@gmail.com>
2022-10-03 11:55:10 -0300

    Update 2_install_noip.sh

--- scripts/2_install_noip.sh
+++ scripts/2_install_noip.sh
@@ -2,29 +2,29 @@
 
 echo "----------[START] 2_install_noip.sh"
 
-echo "create noip folder in /home/pi/noip"
+echo "create no-ip folder in /home/pi/noip"
 cd /home/pi
 mkdir noip
 cd noip
 
-echo "download last version of noip"
+echo "download last version of no-ip"
 wget https://www.noip.com/client/linux/noip-duc-linux.tar.gz
 
-echo "decompress noip"
+echo "decompress no-ip"
 tar -zxvf noip-duc-linux.tar.gz
 
-echo "build and install noip"
+echo "build, install and config no-ip "
 cd noip-2.1.9-1
 sudo make
 sudo make install
 
-echo "configure noip to run on the boot"
+echo "configure no-ip to run on the boot"
 
 echo "write out current crontab"
 sudo crontab -l > pi_root_cron
 
 echo "echo new cron into cron file"
-echo "@reboot /usr/local/bin/noip2" >> pi_root_cron
+echo "@reboot /usr/local/bin/noip2 -c /usr/local/etc/no-ip2.conf" >> pi_root_cron
 
 echo "install new cron file"
 sudo crontab pi_root_cron
