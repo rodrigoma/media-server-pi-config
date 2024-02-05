fb42a5614b0ae3cfe439ac4e2c44e831a7863bcf
Rodrigo Monteiro <rodrigoma@gmail.com>
2024-02-04 21:15:10 -0300

    ajustes nos scripts

--- scripts/07_install_sqlbak.sh
+++ scripts/07_install_sqlbak.sh
@@ -3,12 +3,19 @@
 echo "----------[START] 07_install_sqlbak.sh"
 
 echo "download sqlbak_armhf.deb"
-curl -o sqlbak.deb https://sqlbak.com/download/linux/latest/sqlbak_armhf.deb
+curl -o sqlbak.deb https://sqlbak.com/download/linux/latest/sqlbak_arm64.deb
 
 echo "" && sleep 2
 
 echo "install sqlbak.deb"
 sudo apt-get install ./sqlbak.deb 
+
+echo "" && sleep 2
+
+echo "create mysql mock"
+sudo touch /usr/bin/mysql
+sudo chown root:root /usr/bin/mysql
+sudo chmod +x /usr/bin/mysql
 
 echo "" && sleep 2
 
