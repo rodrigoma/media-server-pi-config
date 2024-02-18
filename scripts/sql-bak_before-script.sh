b65c5f51476a3fa0f8e82e6fac9280445f39b68e
Rodrigo Monteiro do Amaral <rodrigoma@gmail.com>
2024-02-18 09:20:21 -0300

    Create sql-bak_before-script.sh

---
+++ scripts/sql-bak_before-script.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#
+# FILE LOCATION: $HOME/scripts/sql-backup_before-script.sh
+#
+# TURN EXEC: chmod +x $HOME/scripts/sql-backup_before-script.sh
+#
+# To be used in https://sqlbak.com in BEFORE SCRIPT session
+#
+
+docker stop pyload-ng mylar transmission sickchill
+
+sudo service plexmediaserver stop
+
+sleep 5
+
+dpkg --get-selections > $HOME/backups/rasp_pi/Package.list
+
+sudo cp -R /etc/apt/sources.list* $HOME/backups/rasp_pi/
+
+sudo apt-key exportall > $HOME/backups/rasp_pi/Repo.keys
