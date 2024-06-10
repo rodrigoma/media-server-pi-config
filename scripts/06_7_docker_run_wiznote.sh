01d696aa47759027d1b14a64bf84295d54b06bf4
Rodrigo Monteiro do Amaral <rodrigoma@gmail.com>
2024-06-09 22:49:19 -0300

    Create 06_7_docker_run_wiznote.sh

---
+++ scripts/06_7_docker_run_wiznote.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "----------[START] 06_7_docker_run_wiznote.sh"
+
+sudo docker run -d \
+	--name wiznote \
+	--hostname wiznote \
+	--network host \
+	-v /home/pi/CenterMediaNew/SupportApps/wiznote/data:/wiz/storage \
+	-e PUID=1000 \
+	-e PGID=1000 \
+	-e TZ=America/Sao_Paulo \
+	-p 80:80 \
+	-p 9269:9269/udp \
+	wiznote/wizserver
+
+echo "----------[ END ] 06_7_docker_run_wiznote.sh"
